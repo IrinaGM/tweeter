9cbaf06b589aa48c7d86e98a1866db6f7c6dad50
IrinaGM <irina.golionko@gmail.com>
2023-05-18 00:51:13 +0000

    refactor submit functionality, move from submit.js to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -74,3 +74,20 @@ const renderTweets = (tweets) => {
 };
 
 renderTweets(data);
+
+
+/**
+ * * Handles the submit functionality for new tweet
+ */
+
+$(document).ready(function () {
+  //add event listener for tweet submission
+  $("#new-tweet-submit").on("submit", function (event) {
+    //prevent default behavior of the submit event in a form
+    event.preventDefault();
+    // serialize the data and send it to the server
+    $.post("/tweets", $(this).serialize()).done(function (data) {
+      console.log("Success: ", data);
+    });
+  });
+});
